b5f8222f84a06d35b2627466acb9c54f7fbd0172
Rahul <rash235r@gmail.com>
2020-04-19 20:52:24 +0200

    chore: added test setup

---
+++ src/components/templates/main/Main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Main = () => <p>Negotiation Page</p>;
+
+export default Main;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Main from './components/templates/main';
 
 // eslint-disable-next-line no-undef
 const rootElement = document.querySelector('#root');
 
-render(<div>Negotiation App</div>, rootElement);
+render(<Main />, rootElement);
