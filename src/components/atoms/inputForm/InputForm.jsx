4c664effe8a48ea57dc494117c8550c857825bb5
Rahul <rash235r@gmail.com>
2020-04-20 19:10:03 +0200

    chore: fix input field value

--- src/components/atoms/inputForm/InputForm.jsx
+++ src/components/atoms/inputForm/InputForm.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, func } from 'prop-types';
 import styles from './inputForm.styles.scss';
 
 const InputForm = ({ label, id, onClick }) => {
+  const [input, setInput] = useState('');
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <form>
       <label htmlFor={id} className={styles.label}>
         {label}
       </label>
-      <input type="number" id={id} className={styles.input} />
+      <input
+        type="number"
+        id={id}
+        className={styles.input}
+        value={input}
+        onChange={onChangeHandler}
+      />
       <button type="submit" onClick={onClick} className={styles.button}>
         Submit
       </button>
